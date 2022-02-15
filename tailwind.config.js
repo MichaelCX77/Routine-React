04cfb10ebfd4579cbbaecaa22201f96e1e836462
Michael Xavier <maycow_77@hotmail.com>
2022-02-14 21:48:47 -0300

    Limpeza do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purger: [
+      '.src/pages/**/*.{js,ts,jsx,tsx}',
+      '.src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
