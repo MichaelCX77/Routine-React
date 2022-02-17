55aa87fababa14b7d4eecd95bb3c75cf67dc9f66
Michael Xavier <maycow_77@hotmail.com>
2022-02-16 23:50:50 -0300

    Refatoração da DateBar

--- src/components/DateBar.tsx
+++ src/components/DateBar.tsx
@@ -1,20 +1,21 @@
 import ButtonSmall from './buttons/ButtonSmall';
 import React from 'react';
 
-function setButtons(props){
 
+
+function getButton(style){
     return(
-        <React.Fragment>
-            <div className='flex items-center'>
-                <ButtonSmall style="mdiChevronLeft" color="#0891b2"/>
-            </div>
-            <div className='flex items-center'>
-                <span>{props.date}</span>
-            </div>
-            <div className='flex items-center'>
-                <ButtonSmall style="mdiChevronRight" color="#0891b2"/>
-            </div>
-        </React.Fragment>
+        <div className='flex items-center'>
+            <ButtonSmall style={style} color="#0891b2"/>
+        </div>
+    )
+}
+
+function getDate(props){
+    return(
+        <div className='flex items-center'>
+            <span>{props.date}</span>
+        </div>
     )
 }
 
@@ -23,7 +24,10 @@ export default function DateBar(props){
         <div className='mb-6'>
             <div className="flex flex-wrap place-content-between 
                             py-1 border-2 border-gray-400 rounded-md">
-                    {setButtons(props)}
+                
+                {getButton("mdiChevronLeft")}
+                {getDate(props)}
+                {getButton("mdiChevronRight")}
             </div>
         </div>
     )
